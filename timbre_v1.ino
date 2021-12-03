fc7f8b273cf0bb6f8c68466c83408e97180923ae
difo23 <lizandro23difo@gmail.com>
2021-12-03 14:35:59 -0400

    Timbre terminado diferente pin para rele

--- timbre_v1.ino
+++ timbre_v1.ino
@@ -19,13 +19,15 @@ boolean running = true;
 
 // Salidas:
 int timbre= 13;
+int rele = 12;
+
 int buzz = 8;
 // Horas de clase tiempo en minutos
 int times = 10;
 int minutos[] = {55, 50, 50 , 20, 50,  50, 40, 40, 50, 50};
 
 int segundos = 60;
-unsigned long mult = 1000UL;
+unsigned long mult = 1UL;
 int timbre_on_time = 8 * mult;
 
 
@@ -34,7 +36,11 @@ void setup(){
 
   pinMode(button, INPUT);
   pinMode(timbre, OUTPUT);
+  pinMode(rele, OUTPUT);
+  
+  
   digitalWrite(button, HIGH);
+  digitalWrite(rele, HIGH);
   Serial.begin(9600);
 
 }
@@ -48,10 +54,14 @@ void loop() {
   if(button_value && running ) {
     // Boton presionado
 
-
+    Serial.println("Button on and running true");
+    Serial.println("Day start ");
     for(int i = 0; (i < times) && button_value ; i++ ) {
+      
 
       digitalWrite(timbre, HIGH);
+      digitalWrite(rele, LOW);
+      Serial.println("Timbre on");
       for (int thisNote = 0; thisNote < 8; thisNote++) {
 
         // to calculate the note duration, take one second 
@@ -68,36 +78,51 @@ void loop() {
         noTone(buzz);
       }
       digitalWrite(timbre, LOW);
+      digitalWrite(rele, HIGH);
       
-      unsigned long  wait = minutos[i] * segundos * mult;
+      
+      Serial.println("Timbre off");
+      
+      unsigned long wait = minutos[i] * segundos * mult;
       Serial.println(wait);
-      Serial.println(minutos[i]);
-      Serial.println(segundos);
-      Serial.println(mult);
+      
       // Delay hasta el siguiente periodo
       delay( wait );
       button_value = digitalRead(button);
+      Serial.println("Read button reset:");
+      Serial.println(button_value);
     }
-
+    
+    Serial.println("End Day");
     running = false;
-
+    Serial.println("Running set false");
+     delay(500);
 
   } 
   else if(button_value == false){
-
+    
+    
+    Serial.println("Button off, running true y timbre off");
+    delay(500);
     running = true;
     digitalWrite(timbre, LOW);
+    digitalWrite(rele, HIGH);
+    
 
 
   } 
   else if (running == false) {
-
+    Serial.println("Running false y timbre off");
+    delay(500);
     digitalWrite(timbre, LOW);
+    digitalWrite(rele, HIGH);
 
   } 
   else {
-
+    Serial.println("Whatever timbre off");
+    delay(500);
     digitalWrite(timbre, LOW);
+    digitalWrite(rele, HIGH);
   }
 
 
