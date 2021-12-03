a7728cd5548d924235d1617e0109e0f37cb4a3f3
difo23 <lizandro23difo@gmail.com>
2021-12-03 13:11:59 -0400

    Timbre terminado listo para pruebas

--- timbre_v1.ino
+++ timbre_v1.ino
@@ -1,6 +1,15 @@
 
+
+ #include "pitches.h"
 // Proyecto tiembre 
 
+int melody[] = {
+  NOTE_C4, NOTE_G3,NOTE_G3, NOTE_A3, NOTE_G3,0, NOTE_B3, NOTE_C4};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  4, 8, 8, 4,4,4,4,4 };
+  
 // Entradas:
 int button= 2;
 int button_value = 0;
@@ -10,14 +19,13 @@ boolean running = true;
 
 // Salidas:
 int timbre= 13;
-
+int buzz = 8;
 // Horas de clase tiempo en minutos
- const int times = 10;
-int minutos[times] = {
-  55, 50, 50 , 20, 50,  50, 40, 40, 50, 50};
+int times = 10;
+int minutos[] = {55, 50, 50 , 20, 50,  50, 40, 40, 50, 50};
 
 int segundos = 60;
-int mult = 1000;
+unsigned long mult = 1000UL;
 int timbre_on_time = 8 * mult;
 
 
@@ -27,6 +35,7 @@ void setup(){
   pinMode(button, INPUT);
   pinMode(timbre, OUTPUT);
   digitalWrite(button, HIGH);
+  Serial.begin(9600);
 
 }
 
@@ -43,11 +52,30 @@ void loop() {
     for(int i = 0; (i < times) && button_value ; i++ ) {
 
       digitalWrite(timbre, HIGH);
-      delay(timbre_on_time);
-      digitalWrite(timbre, LOW);
+      for (int thisNote = 0; thisNote < 8; thisNote++) {
 
+        // to calculate the note duration, take one second 
+        // divided by the note type.
+        //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+        int noteDuration = 1000/noteDurations[thisNote];
+        tone(buzz, melody[thisNote],noteDuration);
+    
+        // to distinguish the notes, set a minimum time between them.
+        // the note's duration + 30% seems to work well:
+        int pauseBetweenNotes = noteDuration * 1.30;
+        delay(pauseBetweenNotes);
+        // stop the tone playing:
+        noTone(buzz);
+      }
+      digitalWrite(timbre, LOW);
+      
+      unsigned long  wait = minutos[i] * segundos * mult;
+      Serial.println(wait);
+      Serial.println(minutos[i]);
+      Serial.println(segundos);
+      Serial.println(mult);
       // Delay hasta el siguiente periodo
-      delay( times[i] * segundos * mult - timbre_on_time );
+      delay( wait );
       button_value = digitalRead(button);
     }
 
