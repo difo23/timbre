a423a952d2e186902c255cd416831f0365f32821
difo23 <lizandro23difo@gmail.com>
2022-02-26 20:14:42 -0400

    feat: Change the logic and library for rtc clock

--- timbre_v1.ino
+++ timbre_v1.ino
@@ -1,11 +1,16 @@
-#include <TimeLib.h>
-#include "pitches.h"
-
-// Proyecto tiembre 
+#include <Wire.h>
+#include "RTClib.h"
 
 
-// Outputs:
-int rele = 13;
+
+
+RTC_DS1307 rtc;
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+int rele = 6;
+
+
 
 
 // Hours de clase tiempo en minutos
@@ -24,52 +29,63 @@ String Hours[] = {
   "2:40 pm"
 };
 
-String Days[] = {
-  "Empty",
-  "Sunday", 
-  "Monday", 
-  "Tuesday", 
-  "Wednesday", 
-  "Thursday", 
-  "Friday", 
-  "Saturday", 
-};
+
 
 
 
 void setup(){
  
   pinMode(rele, OUTPUT);
-  digitalWrite(rele, HIGH); 
-
-  // LED on for load in the battery mode
-  pinMode(3, OUTPUT);
-  digitalWrite(3, HIGH);
-
-  
+  digitalWrite(rele, HIGH);  
   Serial.begin(9600);
-  resetTime();
+  
+if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    while (1) delay(10);
+  }
+
+ 
+   
+    // When time needs to be set on a new device, or after a power loss, the
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+   
+  
+  
+ 
 }
 
 
+
+
+
+
 void loop() {
+
+  DateTime now = rtc.now();
+
   
-  int weekDay   = weekday();
-  int hourDay   = hour();
-  int minuteDay = minute();
+  
+  int weekDay   = now.day();
+  int hourDay   = now.hour();
+  int minuteDay = now.minute();
+  int secondDay = now.second();
  
   // Button state on, not weekend, not woking hours 
+  String timeNowString = ""+ String(hourDay) +""+ String(minuteDay);
+  int caseSwitch = timeNowString.toInt();
+  Serial.println(caseSwitch);
   
-  if( ( weekDay > 1 && weekDay < 7) && (hourDay > 6 && hourDay < 15 )) {
-    
   
-    String timeNowString = ""+ String(hourDay) +""+ String(minuteDay);
-    int caseSwitch = timeNowString.toInt();
-    Serial.println(caseSwitch);
-    
+  if( ( weekDay > 0 && weekDay < 7) && (hourDay > 6 && hourDay < 15 )) {
+      
     switch (caseSwitch) {
         case 72:
-              ring();     
+              ring();   
         break;
         
         case 80:
@@ -107,16 +123,17 @@ void loop() {
         case 1350:
               ring();
         break;
+        
+        
         case 1440:
               Serial.println("Bye!!, end the day.");
               ring();
         break;
         
         default:
-            Serial.println("Time between hours doorbell off y button on");
+            Serial.println("Time between hours doorbell off");
             printTime();
             digitalWrite(rele, HIGH); 
-            waitMinute();
         break;
     }
    
@@ -124,15 +141,13 @@ void loop() {
 
   } 
   else {
-    Serial.println("Button on in weekend, not working hours and doorbell off");
+    Serial.println("In weekend or not working hours and doorbell off");
     digitalWrite(rele, HIGH);
     printTime();
-    waitMinute();  
   }
   
   
-
-
+  
 }
 
 
@@ -151,48 +166,50 @@ void ring() {
 
 void printTime(){
   
-  Serial.println("Time now: ");
-  Serial.print("Hour: ");
-  Serial.print(hour());
-  Serial.print(":");
-  Serial.print(minute());
-  Serial.print(":");
-  Serial.print(second());
-  
-  Serial.print("  Day:");
-  Serial.print(day());  // the day now (1-31)
-  Serial.print(" ( ");
-  Serial.print(Days[weekday()]); // day of the week (1-7), Sunday is day 1
-  Serial.print(" )");
-  Serial.print(" Month:");
-  Serial.print(month()); // the month now (1-12)
-  Serial.print(" Year:");
-  Serial.println(year()); // the full four digit year: (2009, 2010 etc)
+  DateTime now = rtc.now();
 
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(" (");
+  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+  Serial.print(") ");
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  Serial.println();
+ 
+  delay(1000);
 }
 
-void resetTime() {
-  // setTime(Hour, minutes, seconds, day, month, year);
-  setTime(11,44 , 0, 14, 1, 2022);
-}
 
 
 void waitMinute() {
+   DateTime now = rtc.now();
 
-  int minuteWait = minute();
+
+  int minuteWait = now.minute();
   
-  while(minuteWait == minute()){
-   
+  while(minuteWait == now.minute()){
+    delay(1000);
   }
+   
+   
 }
 
 
 void waitRing() {
+    
+  DateTime now = rtc.now();
+  
+  int waitSeconds = now.second();
 
-  int waitSeconds = second();
- 
-  while((second() - waitSeconds)  < 6){
-   
+  while((now.second() - waitSeconds)  < 6){
+    delay(1000);
   }
-
+  
 }
